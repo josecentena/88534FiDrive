675a51373a63b2efeea92422e3166bcee2e19014
josecentena <tonycipo50@gmail.com>
2020-09-14 13:49:50 -0300

    agregar primera parte

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+echo "Esta es una aplicación para evaluar mis progresos en la materia PWD 2020.";
+echo "Mis nombres es Jose Antonio Centena.";
+echo "Mi Nro de Legajo es 88534.";
+
+?>
---
+++ proyecto.php
@@ -0,0 +1,5 @@
+<?php
+
+echo "Hola Cipolletti!";
+
+?>
